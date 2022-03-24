afb1876d1134dd85aa14e19ada358a1251251dc7
Andrei Nistor <45632814+ianistor@users.noreply.github.com>
2022-03-24 08:06:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,14 @@ from soco import SoCo
 
 if __name__ == '__main__':
     sonos = SoCo('192.168.1.229') # Pass in the IP of your Sonos speaker
-    # You could use the discover function instead, if you don't know the IP
-
-    # Pass in a URI to a media file to have it streamed through the Sonos
-    # speaker
 
     sonos.volume = 50
 
     sonos.play_uri('http://192.168.1.45:8000/sounds/caged_intro_2.mp3')
     sonos.play()
-    # print ("Done with first")
+
     sleep(12)
-    # print ("done with sleep")
+
     sonos.play_uri('http://192.168.1.45:8000/sounds/caged_message_new_0.mp3')
     sonos.play()
 
@@ -25,5 +21,4 @@ if __name__ == '__main__':
 
     # sonos.pause()
 
-    # print (sonos.volume)
-    # Play a stopped or paused track
+
